78cad468b89f125b53f7faaa29524e1fb926630b
Dominic Chu <dominic.m.chu@gmail.com>
2019-12-18 14:24:13 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+let divide = function() {
+    let answer = 2000 / 100
+    return answer
+}
+
+let square = (number) => {
+    return (number * number)
+}
+
+let add = (param1, param2) => {
+    return (param1 + param2)
+}
